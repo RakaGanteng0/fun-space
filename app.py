9c2a43781e9621d816c40e7756065bcb23addb80
RakaGanteng0 <140138894+RakaGanteng0@users.noreply.github.com>
2023-07-21 13:35:11 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,3 +4,10 @@ def luas_segitiga():
     luas = a * t / 2
     print ("Luas segitiga adalah:", luas);
     luas_segitiga()
+def luas_persegi_panjang():
+    p = int(input("Masukan panjang persegi: "))
+    l = int(input("Masukan lebar persegi: "))
+    luas = p * 1
+    print("Luas Persegi adalah: ", Luas)
+
+    luas_persegi_panjang()
